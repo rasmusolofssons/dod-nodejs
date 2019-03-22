a035c42f18807e2c15620cdc2d7aaa4ee6ceb2a8
Rasmus Olofsson <rasmus@bazooka.se>
2019-03-22 19:08:07 +0100

    Player can move using arrow keys

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const Map = require('./src/map');
 const Keypress = require('keypress');
+const Player = require('./src/player');
+const Monster = require('./src/monster');
 
-
+let player = new Player("Rasmus");
+let monster = new Monster("Monster");
 let map = new Map(10,10);
-map.draw();
+map.draw(player,monster);
 
 // make `process.stdin` begin emitting "keypress" events
 Keypress(process.stdin);
@@ -11,7 +14,7 @@ Keypress(process.stdin);
 // listen for the "keypress" event
 process.stdin.on('keypress', function (ch, key) {
     if(key){
-        map.draw(key.name);
+        map.draw(player,monster, key.name);
     }
   if (key && key.ctrl && key.name == 'c') {
     process.stdin.pause();
--- src/map.js
+++ src/map.js
@@ -7,9 +7,9 @@ class Map {
         this.height = height;
     }
 
-    draw (key){
-        let player = new Player("Rasmus");
-        let monster = new Monster("Monster");
+    draw (player, monster, key){
+        this.playerMove(player, key);
+      console.log('Key: ' + key);
         console.log(`Player X: ${player.x} \n Player Y: ${player.y}`);
         //console.log(`Monster X: ${monster.x} \n Monster Y: ${monster.y}`);
          let map = [];
@@ -17,12 +17,7 @@ class Map {
          for(let y = 0; y < this.height; y++){
             map[y] = [];
              for(let x = 0; x < this.width; x++){
-                //  if(y === player.y && x === player.x){
-                //         map[y][x] = 'p';
-                //      }
-                //      else if(y === monster.y && x === monster.x){
-                //         map[y][x] = 'm';
-                //      }
+             
                 if(this.characterHere(player, y, x)){
                     map[y][x] = player.symbol;
                 } 
@@ -41,6 +36,21 @@ class Map {
      characterHere(character, y, x){
         return (y === character.y && x === character.x);
 
+    }
+
+    playerMove(player, key){
+        if(key === 'right'){
+            player.x = player.x + 1;
+        }
+        else if (key === 'left'){
+            player.x = player.x -1;
+        }
+        else if (key === 'up'){
+            player.y = player.y-1;
+        }
+        else if (key === 'down'){
+            player.y = player.y +1;
+        }
     }
 }
 
