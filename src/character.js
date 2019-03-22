a62ea44b678224075c060c4e3cccfa2ccc286db8
Rasmus Olofsson <rasmus@bazooka.se>
2019-03-22 19:07:51 +0100

    Characters don't spawn outside array

--- src/character.js
+++ src/character.js
@@ -1,8 +1,8 @@
 class Character {
     constructor(name){
         this.name = name;
-        this.x = Math.floor(Math.random() * 10) + 1;
-        this.y = Math.floor(Math.random() * 10) + 1;
+        this.x = Math.floor(Math.random() * 10);
+        this.y = Math.floor(Math.random() * 10);
     }
 }
 
